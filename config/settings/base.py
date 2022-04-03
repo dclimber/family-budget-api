03ab45b5fa5d94d747ef3404282efd462b985da3
Dastan Abd <dastand.climber@gmail.com>
2022-04-03 15:31:30 +0600

    feat: Add static root setting

--- config/settings/base.py
+++ config/settings/base.py
@@ -100,8 +100,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = BASE_DIR / '..' / '..' / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
